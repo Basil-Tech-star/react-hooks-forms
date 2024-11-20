0ef030f031086b825cea9fc699966d35529b1393
Basil Wabuke <babazwabuke4@gmail.com>
2024-11-21 01:55:41 +0300

    adding changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './components/Form'
 import ParentComponent from './components/ParentComponent.js';
 
 ReactDOM.render(
