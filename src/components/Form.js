9c7d4efcf8da79a4d20579d6d5416b4e108525f8
Basil Wabuke <babazwabuke4@gmail.com>
2024-11-21 01:50:35 +0300

    updating to show controlled forms

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+function Form({
+  firstName,
+  lastName,
+  handleFirstNameChange,
+  handleLastNameChange,
+}) {
+  const [number, setNumber] = useState(0);
+  const [isInvalid, setIsInvalid] = useState(null);
 
+  function handleNumberChange(event) {
+    const newNumber = parseInt(event.target.value);
+    if (newNumber >= 0 && newNumber <= 5) {
+      setNumber(newNumber);
+      setIsInvalid(null);
+    } else {
+      setIsInvalid(`${newNumber} is not a valid number between 0 and 5`);
+    }
+  }
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="number" onChange={handleNumberChange} value={number} />
+      {isInvalid ? <span style={{color: "red"}}>{isInvalid}</span>: null}
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
